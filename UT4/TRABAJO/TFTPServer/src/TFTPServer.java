efba4fa8581089224e2a2b96402c6cc39912cc81
Alejandro Matias <125260169+AlejandroMGDAM2@users.noreply.github.com>
2023-11-26 16:01:52 +0100

    Revision Server

--- UT4/TRABAJO/TFTPServer/src/TFTPServer.java
+++ UT4/TRABAJO/TFTPServer/src/TFTPServer.java
@@ -6,30 +6,30 @@ public class TFTPServer {
 
 	public static void main(String argv[]) {
 		try {
-			//use port 6973
-			DatagramSocket sock = new DatagramSocket(6973);
-			System.out.println("Server Ready.  Port:  " + sock.getLocalPort());
+			// Usar puerto 6973
+			DatagramSocket socket = new DatagramSocket(6973);
+			System.out.println("Servidor listo. Puerto: " + socket.getLocalPort());
 
-			// Listen for requests
+			// Escuchar solicitudes
 			while (true) {
-				TFTPpacket in = TFTPpacket.receive(sock);
-				// receive read request
-				if (in instanceof TFTPread) {
-					System.out.println("Read Request from " + in.getAddress());
-					TFTPserverRRQ r = new TFTPserverRRQ((TFTPread) in);
+				TFTPpacket entrada = TFTPpacket.receive(socket);
+				// Recibir solicitud de lectura
+				if (entrada instanceof TFTPread) {
+					System.out.println("Solicitud de lectura desde " + entrada.getAddress());
+					TFTPserverRRQ r = new TFTPserverRRQ((TFTPread)entrada);
 				}
-				// receive write request
-				else if (in instanceof TFTPwrite) {
-					System.out.println("Write Request from " + in.getAddress());
-					TFTPserverWRQ w = new TFTPserverWRQ((TFTPwrite) in);
+				// Recibir solicitud de escritura
+				else if (entrada instanceof TFTPwrite) {
+					System.out.println("Solicitud de escritura desde " + entrada.getAddress());
+					TFTPserverWRQ w = new TFTPserverWRQ((TFTPwrite) entrada);
 				}
 			}
 		} catch (SocketException e) {
-			System.out.println("Server terminated(SocketException) " + e.getMessage());
+			System.out.println("Servidor terminado (SocketException): " + e.getMessage());
 		} catch (TftpException e) {
-			System.out.println("Server terminated(TftpException)" + e.getMessage());
+			System.out.println("Servidor terminado (TftpException): " + e.getMessage());
 		} catch (IOException e) {
-			System.out.println("Server terminated(IOException)" + e.getMessage());
+			System.out.println("Servidor terminado (IOException): " + e.getMessage());
 		}
 	}
 }
